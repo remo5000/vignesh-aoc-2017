44eb22f3e504f855fd79877145287c5a533379f9
remo5000 <vigneshshankar@gmail.com>
2017-12-07 16:46:20 +0800

    day 3a first working solution

--- day_3a.py
+++ day_3a.py
@@ -1,19 +1,13 @@
-target = 12
+target = 312051
 k = 1
 # TODO: replace with mad cow + binary search
-while k**2 < target:
+while k ** 2 < target:
 	k += 2
-nearest_to_one = k**2 - ((k - 1) // 2)
-print("nearest_to_one", nearest_to_one)
+nearest_to_one = k ** 2 - ((k - 1) // 2)
 distance_to_one = (k - 1) // 2
-print("distance_to_one", distance_to_one)
-distance_to_nearest_number = 0
-while nearest_to_one - target >= 0 and target != 1:
-	distance_to_nearest_number = nearest_to_one - target
-	print("distance_to_nearest_number", distance_to_nearest_number)
+while abs(nearest_to_one - target) > ((k - 1) // 2):
 	nearest_to_one -= (k - 1)
-	print("new nearest_to_one", nearest_to_one)
+distance_to_nearest_number = abs(nearest_to_one - target)
 distance_to_one += distance_to_nearest_number
-
 print(distance_to_one)
 
