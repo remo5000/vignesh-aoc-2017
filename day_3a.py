e5eb7dafa1a3d61322ddcf584e236ea3fb515bfd
remo5000 <vigneshshankar@gmail.com>
2017-12-07 16:50:00 +0800

    day_3a refactor variables

--- day_3a.py
+++ day_3a.py
@@ -3,10 +3,12 @@ k = 1
 # TODO: replace with mad cow + binary search
 while k ** 2 < target:
 	k += 2
-nearest_to_one = k ** 2 - ((k - 1) // 2)
-distance_to_one = (k - 1) // 2
-while abs(nearest_to_one - target) > ((k - 1) // 2):
-	nearest_to_one -= (k - 1)
+max_dist_to_one = k - 1
+min_dist_to_one = max_dist_to_one // 2
+nearest_to_one = k ** 2 - min_dist_to_one
+distance_to_one = min_dist_to_one
+while abs(nearest_to_one - target) > min_dist_to_one:
+	nearest_to_one -= max_dist_to_one
 distance_to_nearest_number = abs(nearest_to_one - target)
 distance_to_one += distance_to_nearest_number
 print(distance_to_one)
