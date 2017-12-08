7df476651fc761578e6a04f06f1294d1b5ec99b6
remo5000 <vigneshshankar@gmail.com>
2017-12-09 00:09:24 +0800

    day 4a add naive solution for testing
    same result

--- day_4a.py
+++ day_4a.py
@@ -13,5 +13,17 @@ for passcode in all_passcodes:
 			word_dict[word] = 0
 	if valid:
 		valid_passcode_count += 1
+
+# for passcode in all_passcodes:
+# 	print(passcode)
+# 	valid = True
+# 	for word in passcode:
+# 		passcode.remove(word)
+# 		if word in passcode:
+# 			valid = False
+# 			break
+# 	if valid:
+# 		valid_passcode_count += 1
+
 print(valid_passcode_count)
 
