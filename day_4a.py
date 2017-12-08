0a5fe97c33962f33e6e01a85369c0f709b8a5e22
remo5000 <vigneshshankar@gmail.com>
2017-12-09 00:22:48 +0800

    day 4a add set solution

--- day_4a.py
+++ day_4a.py
@@ -2,18 +2,25 @@ from given_inputs import day_4a_final
 
 all_passcodes = [passcode_string.split() for passcode_string in day_4a_final.splitlines()]
 valid_passcode_count = 0
+
 for passcode in all_passcodes:
-	word_dict = {}
-	valid = True
-	for word in passcode:
-		if word in word_dict:
-			valid = False
-			break
-		else:
-			word_dict[word] = 0
-	if valid:
+	if len(passcode) == len(set(passcode)):
 		valid_passcode_count += 1
 
+# # Dictionary Method
+# for passcode in all_passcodes:
+# 	word_dict = {}
+# 	valid = True
+# 	for word in passcode:
+# 		if word in word_dict:
+# 			valid = False
+# 			break
+# 		else:
+# 			word_dict[word] = 0
+# 	if valid:
+# 		valid_passcode_count += 1
+
+# # Naive Method
 # for passcode in all_passcodes:
 # 	print(passcode)
 # 	valid = True
