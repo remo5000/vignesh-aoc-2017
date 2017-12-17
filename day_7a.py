f22cceba4c0651d2b5d372b9cd388507f0b4d325
remo5000 <vigneshshankar@gmail.com>
2017-12-17 21:14:12 +0800

    day 7a abstract to function

--- day_7a.py
+++ day_7a.py
@@ -1,12 +1,16 @@
 from given_inputs import day_7a_final
-names = [line.split(' ', 1)[0] for line in day_7a_final.splitlines()]
-number_of_parents = {name: 0 for name in names}
-for line in day_7a_final.splitlines():
-	if '->' not in line:
-		continue
-	children = line.split('-> ')[1].split(', ')
-	for child in children:
-		number_of_parents[child] += 1
-for program in number_of_parents:
-	if number_of_parents[program] == 0:
-		print(program)
+
+def find_parent(input):
+	names = [line.split(' ', 1)[0] for line in day_7a_final.splitlines()]
+	number_of_parents = {name: 0 for name in names}
+	for line in day_7a_final.splitlines():
+		if '->' not in line:
+			continue
+		children = line.split('-> ')[1].split(', ')
+		for child in children:
+			number_of_parents[child] += 1
+	for program in number_of_parents:
+		if number_of_parents[program] == 0:
+			return program
+
+print(find_parent(day_7a_final))
