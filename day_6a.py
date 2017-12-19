e923684bb72a610ceb866e9ad6aceb2661da1929
remo5000 <vigneshshankar@gmail.com>
2017-12-20 01:26:05 +0800

    day 6a first working solution

--- day_6a.py
+++ day_6a.py
@@ -1,48 +1,45 @@
-from given_inputs import day_6a_test
-
-arr = [int(c) for c in  day_6a_test.split()]
-previously_seen = {}
-redist_count = 0
-reached_infinite_loop = False
+from given_inputs import day_6a_final as input_str
 
 def max_element_index(arr):
-    max_index = 0
-    max_num = arr[max_index]
-    for index, num in enumerate(arr):
-        if num >= max_num:
-            max_num = num
-            max_index = index
-    return max_index
+    return arr.index(max(arr))
+
+def brute_distribute(max_index, arr):
+    i = max_index
+    max_val = arr[i]
+    while max_val:
+        i = (i + 1) % len(arr)
+        arr[i] += 1
+        max_val -= 1
+    arr[max_index] = max_val
 
 def distribute(max_index, arr):
-    print("max is", arr[max_index], "arr is", arr, "len is", len(arr)) 
-    while arr[max_index] >= len(arr) - 1:
-        for index in range(len(arr)):
-            if index == max_index:
-                continue
-            else:
-                arr[index] += 1
-        # for index_to_add in range(max_index + 1, max_index + len(arr)):
-        #     arr[index_to_add % len(arr)] += 1
-        # arr[max_index] -= (len(arr) - 1)
+    max_val = arr[max_index]
+    increment = max_val // (len(arr) - 1)
+    max_val = max_val % (len(arr) - 1)
+    for i in range(len(arr)):
+        if i == max_index:
+            arr[i] = max_val
+        else:
+            arr[i] += increment
+    brute_distribute(max_index, arr)
 
-def seen_before(arr):
+def have_seen_before(arr):
     return ",".join([str(i) for i in arr]) in previously_seen
 
 def add_to_seen_before(arr):
-    previously_seen[",".join([str(i) for i in arr])] = True
+    previously_seen.add(",".join([str(i) for i in arr]))
 
+arr = [int(c) for c in  input_str.split()]
+previously_seen = set()
+reached_infinite_loop = False
 add_to_seen_before(arr)
+
 while not reached_infinite_loop:
-    # print("array", arr, "previously_seen", previously_seen)    
     max_index = max_element_index(arr)
     distribute(max_index, arr)
-    # print("after redist,", arr)
-    redist_count += 1
-    if seen_before(arr):
-        print(arr, "is in", previously_seen)
+    if have_seen_before(arr):
         reached_infinite_loop = True
     else:
         add_to_seen_before(arr)
 
-print(redist_count)
+print(len(previously_seen))
