16d44b899b71e4f0c63dd14cd63b3ad995524a24
remo5000 <vigneshshankar@gmail.com>
2017-12-25 21:56:24 +0800

    12a first working solution

--- day_12a.py
+++ day_12a.py
@@ -1,14 +1,14 @@
-from given_inputs import day_12a_test as input_str
+from given_inputs import day_12a_final as input_str
 from functools import reduce
 
-def connected_programs_helper(program_ls, prev_seen_set):
-    program_ls = list(filter(lambda x: x not in prev_seen_set, program_ls))
-    print("unseen", program_ls, "seen", prev_seen_set)
-    if program_ls == []:
+def connected_programs_helper(program_ls, prev_seen):
+    print("unseen", program_ls, "seen", prev_seen)
+    programs = filter(lambda x: x not in prev_seen, program_ls)
+    if len(program_ls) == 0:
         return set()
     else:
         trawl = lambda seen, p: seen | connected_programs_helper(register[p], seen | set([p]))
-        all_seen = reduce(trawl, program_ls, prev_seen_set)
+        all_seen = reduce(trawl, list(programs), prev_seen)
         return all_seen
 
 def connected_programs(first_program):
