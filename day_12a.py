75a47e568c2e9e3e4815c6efae7c1cda6bfbfd8b
remo5000 <vigneshshankar@gmail.com>
2017-12-25 18:26:38 +0800

    12a change accumulate_network to connected_programs

--- day_12a.py
+++ day_12a.py
@@ -1,12 +1,12 @@
 from given_inputs import day_12a_test as input_str
 from functools import reduce
 
-def accumulate_network(program_ls, prev_seen):
+def connected_programs(program_ls, prev_seen):
     if program_ls == []:
         return []
     else:
         not_seen = map(lambda x: x not in prev_seen, program_ls)
-        trawl = lambda seen, p: seen + accumulate_network(register[p], seen)
+        trawl = lambda seen, p: seen + connected_programs(register[p], seen)
         all_seen = reduce(trawl, not_seen, prev_seen)
         return all_seen
 
@@ -15,5 +15,5 @@ for line in input_str.splitlines():
     program = line.split(" <-> ")[0]
     incident_programs = line.split(" <-> ")[1].split(", ")
     register[program] = incident_programs
-connected_to_zero = accumulate_network(register['0'], ['0'])
+connected_to_zero = connected_programs(register['0'], ['0'])
 print(len(connected_to_zero))
