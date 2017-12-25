67d4915ff03eaefb272df3aedca08926971e3222
remo5000 <vigneshshankar@gmail.com>
2017-12-25 23:24:36 +0800

    mod 12b use 12a functions

--- day_12a.py
+++ day_12a.py
@@ -1,24 +1,26 @@
 from given_inputs import day_12a_final as input_str
 from functools import reduce
 
-def connected_programs_helper(program_ls, prev_seen):
+def connected_programs_helper(program_ls, prev_seen, register):
     if len(program_ls) == 0:
         return set()
     else:
         prev_seen = prev_seen | set(program_ls)
         connected_programs = map(lambda x: register[x], program_ls)
         connected_programs = reduce(lambda a, x: a + x, connected_programs, [])
-        unseen_programs = list(set(connected_programs) - prev_seen)
-        return prev_seen | connected_programs_helper(unseen_programs, prev_seen)
+        unseen = list(set(connected_programs) - prev_seen)
+        return prev_seen | connected_programs_helper(unseen, prev_seen, register)
 
-def connected_programs(first_program):
-    return connected_programs_helper(register[first_program], set([first_program]))
+def connected_programs(first_program, register):
+    program_ls = register[first_program]
+    prev = set([first_program])
+    return connected_programs_helper(program_ls, prev, register)
 
 register = {}
 for line in input_str.splitlines():
     program = line.split(" <-> ")[0]
     incident_programs = line.split(" <-> ")[1].split(", ")
     register[program] = incident_programs
-connected_to_zero = connected_programs('0')
+connected_to_zero = connected_programs('0', register)
 print(connected_to_zero)
 print(len(connected_to_zero))
