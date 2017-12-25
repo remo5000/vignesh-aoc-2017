edf06aa855fdab88c723b285c4460f3ca104b9d9
remo5000 <vigneshshankar@gmail.com>
2017-12-25 22:34:50 +0800

    12a changed to set and functional method

--- day_12a.py
+++ day_12a.py
@@ -1,15 +1,15 @@
-from given_inputs import day_12a_final as input_str
+from given_inputs import day_12a_test as input_str
 from functools import reduce
 
 def connected_programs_helper(program_ls, prev_seen):
-    print("unseen", program_ls, "seen", prev_seen)
-    programs = filter(lambda x: x not in prev_seen, program_ls)
     if len(program_ls) == 0:
         return set()
     else:
-        trawl = lambda seen, p: seen | connected_programs_helper(register[p], seen | set([p]))
-        all_seen = reduce(trawl, list(programs), prev_seen)
-        return all_seen
+        prev_seen = prev_seen | set(program_ls)
+        connected_programs = map(lambda x: register[x], program_ls)
+        connected_programs = reduce(lambda a, x: a + x, connected_programs, [])
+        unseen_programs = list(set(connected_programs) - prev_seen)
+        return prev_seen | connected_programs_helper(unseen_programs, prev_seen)
 
 def connected_programs(first_program):
     return connected_programs_helper(register[first_program], set([first_program]))
