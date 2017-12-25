2ade9de46e19883aa01f0afb0ed98cac3197cdd1
remo5000 <vigneshshankar@gmail.com>
2017-12-25 21:39:03 +0800

    12a wip changd to set

--- day_12a.py
+++ day_12a.py
@@ -1,19 +1,24 @@
 from given_inputs import day_12a_test as input_str
 from functools import reduce
 
-def connected_programs(program_ls, prev_seen):
+def connected_programs_helper(program_ls, prev_seen_set):
+    program_ls = list(filter(lambda x: x not in prev_seen_set, program_ls))
+    print("unseen", program_ls, "seen", prev_seen_set)
     if program_ls == []:
-        return []
+        return set()
     else:
-        not_seen = map(lambda x: x not in prev_seen, program_ls)
-        trawl = lambda seen, p: seen + connected_programs(register[p], seen)
-        all_seen = reduce(trawl, not_seen, prev_seen)
+        trawl = lambda seen, p: seen | connected_programs_helper(register[p], seen | set([p]))
+        all_seen = reduce(trawl, program_ls, prev_seen_set)
         return all_seen
+
+def connected_programs(first_program):
+    return connected_programs_helper(register[first_program], set([first_program]))
 
 register = {}
 for line in input_str.splitlines():
     program = line.split(" <-> ")[0]
     incident_programs = line.split(" <-> ")[1].split(", ")
     register[program] = incident_programs
-connected_to_zero = connected_programs(register['0'], ['0'])
+connected_to_zero = connected_programs('0')
+print(connected_to_zero)
 print(len(connected_to_zero))
