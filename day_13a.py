b2c8b26f9c936669e94ed13b83f1bdecfe5aba64
remo5000 <vigneshshankar@gmail.com>
2017-12-26 21:27:26 +0800

    13a changed to 0 index again

--- day_13a.py
+++ day_13a.py
@@ -1,11 +1,13 @@
-from given_inputs import day_13a_test as input_str
+from given_inputs import day_13a_final as input_str
 
+# 6 4   m3
 def caught(depth, rng):
-    depth += 1
-    scanner_depth = (depth % ((rng * 2) - 1))
-    if scanner_depth > rng:
-        scanner_depth = rng - (scanner_depth % rng)
-    return scanner_depth == 1
+    max_rng = rng - 1
+    scanner_depth = depth % ((rng * 2) - 1)
+    if scanner_depth > max_rng:
+        excess = scanner_depth - max_rng
+        scanner_depth = max_rng - excess
+    return scanner_depth == 0
 
 lines = input_str.splitlines()
 severity = 0
@@ -13,5 +15,6 @@ for line in input_str.splitlines():
     depth = int(line.split(": ")[0])
     scan_range = int(line.split(": ")[1])
     if caught(depth, scan_range):
+        print(depth, scan_range)
         severity += (depth * scan_range)
 print(severity)
